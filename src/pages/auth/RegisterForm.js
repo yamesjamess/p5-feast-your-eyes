17b801384109a9f640222a74b5a88054599b2628
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-14 21:56:59 +0000

    refactor: renaming the SignInUpForm css to match with the rest of the project terminology

--- src/pages/auth/RegisterForm.js
+++ src/pages/auth/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
-import styles from "../../styles/SignInUpForm.module.css";
+import styles from "../../styles/RegisterSignInForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
