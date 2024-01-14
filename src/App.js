82ee42bb73f3cead551ea745f4d00e842c7353f4
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-14 16:21:16 +0000

    feat: add react bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Feast Your Eyes</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
