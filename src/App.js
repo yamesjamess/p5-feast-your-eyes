afa3fe9fcd808e4551c62758c42f4498a6656ab4
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-14 16:35:26 +0000

    style Create style for App.js nad NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Feast Your Eyes</h1>
+    <div className={styles.App}>
       <NavBar />
-      <Button variant="primary">Primary</Button>
     </div>
   );
 }
