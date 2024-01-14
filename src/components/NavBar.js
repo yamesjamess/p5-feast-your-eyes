4b7ca3eb21b133d63c82e57ab341c0a450eafea2
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-14 16:38:11 +0000

    feat: Add Router to the project to handle links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,29 @@ import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link>
-              <i className="fa-solid fa-house"></i> home
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fa-solid fa-arrow-right-to-bracket"></i> sign in
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fa-solid fa-user-plus"></i> sign up
-            </Nav.Link>
+            <NavLink exact to="/" activeClassName={styles.Active}>
+              <i className="fa-solid fa-house"></i>home
+            </NavLink>
+            <NavLink to="/signin" activeClassName={styles.Active}>
+              <i className="fa-solid fa-arrow-right-to-bracket"></i>sign in
+            </NavLink>
+            <NavLink to="/signup" activeClassName={styles.Active}>
+              <i className="fa-solid fa-user-plus"></i>sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
