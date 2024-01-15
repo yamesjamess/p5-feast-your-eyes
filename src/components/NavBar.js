af6a0d3747e7f204d26bf44b052cfdc8667f35d2
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-15 22:44:51 +0000

    feat: create variables to store different types of icons that will display to user depending on if they're logged in or not

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,64 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleLogOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const addPostIcon = (
+    <NavLink to="/posts/create" activeClassName={styles.Active}>
+      <i className="fa-solid fa-plus"></i>add post
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink to="/feed" activeClassName={styles.Active}>
+        <i className="fa-solid fa-pizza-slice"></i>feed
+      </NavLink>
+      <NavLink to="/liked" activeClassName={styles.Active}>
+        <i className="fa-solid fa-face-grin-hearts"></i>liked
+      </NavLink>
+      <NavLink to="/recommended" activeClassName={styles.Active}>
+        <i className="fa-solid fa-star"></i>recommended
+      </NavLink>
+      <NavLink to="/" onClick={handleLogOut}>
+        <i className="fa-solid fa-arrow-right-from-bracket"></i>logout
+      </NavLink>
+      <NavLink to={`/profiles/${currentUser?.profile_id}`}>
+        <Avatar
+          src={currentUser?.profile_image}
+          text={currentUser?.username}
+          height={30}
+        />
+      </NavLink>
+    </>
+  );
+  const loggedOutIcons = (
+    <>
+      <NavLink to="/signin" activeClassName={styles.Active}>
+        <i className="fa-solid fa-arrow-right-to-bracket"></i>sign in
+      </NavLink>
+      <NavLink to="/register" activeClassName={styles.Active}>
+        <i className="fa-solid fa-user-plus"></i>register
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -13,18 +69,14 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <NavLink exact to="/" activeClassName={styles.Active}>
               <i className="fa-solid fa-house"></i>home
             </NavLink>
-            <NavLink to="/signin" activeClassName={styles.Active}>
-              <i className="fa-solid fa-arrow-right-to-bracket"></i>sign in
-            </NavLink>
-            <NavLink to="/register" activeClassName={styles.Active}>
-              <i className="fa-solid fa-user-plus"></i>register
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
