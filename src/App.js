57e505fb96ecc891ba27a899bb44400dd4c1ed09
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-15 21:44:34 +0000

    refactor: refactor currentuser context into a CurrentUserProvider

--- src/App.js
+++ src/App.js
@@ -5,44 +5,20 @@ import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import "./api/axiosDefaults";
 import RegisterForm from "./pages/auth/RegisterForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { createContext, useEffect, useState } from "react";
-import axios from "axios";
-
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get("dj-rest-auth/user/");
-      setCurrentUser(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    handleMount();
-  }, []);
-
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
-              <Route exact path="/signin" render={() => <SignInForm />} />
-              <Route exact path="/register" render={() => <RegisterForm />} />
-              <Route render={() => <h1>Error 404 - Page not found!</h1>} />
-            </Switch>
-          </Container>
-        </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/register" render={() => <RegisterForm />} />
+          <Route render={() => <h1>Error 404 - Page not found!</h1>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
