5ef6d126bffe5da29b353a6cb11240ebdb33e680
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-15 22:08:22 +0000

    feat: create axios interceptor for request and response to refresh user tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://fye-drf-api-6b84783d9a37.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+// Interceptor to refresh tokens - one for request and one for response
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
