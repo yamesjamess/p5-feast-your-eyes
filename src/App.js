c87097a180aa01402642bc373029fb87d15b4ebc
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-16 21:25:14 +0000

    feat: create a Route for PostCreateForm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import "./api/axiosDefaults";
 import RegisterForm from "./pages/auth/RegisterForm";
 import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/register" render={() => <RegisterForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route render={() => <h1>Error 404 - Page not found!</h1>} />
         </Switch>
       </Container>
