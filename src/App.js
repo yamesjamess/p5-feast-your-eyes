06b081ce6f371eab44b6d1bcd57f8fa3c454ce8b
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-17 22:14:51 +0000

    refactor: The home page to display PostsListPage component instead

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ import RegisterForm from "./pages/auth/RegisterForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostDetailPage from "./pages/posts/PostDetailPage";
+import PostsListPage from "./pages/posts/PostsListPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => <PostsListPage message="sorry! no results found!" />}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/register" render={() => <RegisterForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
