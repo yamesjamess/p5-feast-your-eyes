cfe13a427f153400f614e2607bcf741884fce501
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-18 22:12:10 +0000

    feat: Create DropDownOptions to show user edit and delete options for their post

---
+++ src/components/DropDownOptions.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import styles from "../styles/DropDownOptions.module.css";
+
+const Caret = React.forwardRef(({ onClick }, ref) => (
+  <i
+    className="fa-solid fa-angle-down"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+export const DropDownOptions = ({ handleEdit, handleDelete }) => {
+  return (
+    <Dropdown className={`ml-auto`} drop="left">
+      <Dropdown.Toggle as={Caret} />
+
+      <Dropdown.Menu
+        className={`${styles.DropDownOptions} text-center`}
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleEdit}
+          aria-label="edit"
+        >
+          <i className="fa-solid fa-pen-to-square" />
+        </Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleDelete}
+          aria-label="delete"
+        >
+          <i className="fa-solid fa-trash-can" />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
