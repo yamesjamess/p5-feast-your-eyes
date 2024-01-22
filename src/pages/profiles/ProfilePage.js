d1611de85a9bef9f690ec63f4fedfc5e10d954eb
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-22 23:43:16 +0000

    feat: add DropDownOptions to ProfilePage component

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -23,6 +23,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import Post from "../posts/Post";
 import NoResults from "../../assets/no-results.png";
+import { ProfileEditDropdown } from "../../components/DropDownOptions";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -57,6 +58,7 @@ function ProfilePage() {
 
   const mainProfile = (
     <>
+      {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           <Image
