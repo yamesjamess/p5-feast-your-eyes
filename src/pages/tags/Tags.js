3cd336595ac0bce580632efe43790e5eae46a12c
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-25 15:04:44 +0000

    feat: filter out unwanted tag and map them to the front end

--- src/pages/tags/Tags.js
+++ src/pages/tags/Tags.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import appStyles from "../../App.module.css";
+import styles from "../../styles/Tags.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const Tags = () => {
+  const [tags, setTags] = useState({ allTags: { results: [] } });
+  const { allTags } = tags;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axiosReq.get("/posts");
+        setTags((prevState) => ({
+          ...prevState,
+          allTags: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const uniqueTags = Array.from(
+    new Set(
+      allTags?.results
+        .map((post) => post.tag.trim())
+        .filter((tag) => tag !== "")
+    )
+  );
+
   return (
     <>
-      <Container className={appStyles.Content}>
-        <div>filter by cuisine type</div>
-        <br />
+      <Container className={`${appStyles.Content} ${styles.TagContainer}`}>
+        <div>filter by tags</div>
+        <hr />
         <ul>
-            <li>Tag names here</li>
-            <li>Tag names here</li>
-            <li>Tag names here</li>
-            <li>Tag names here</li>
-            <li>Tag names here</li>
-            <li>Tag names here</li>
-            <li>Tag names here</li>
-            <li>Tag names here</li>
-            <li>Tag names here</li>
+          {uniqueTags.map((tag, index) => (
+            <li key={index}>{tag}</li>
+          ))}
         </ul>
-        
       </Container>
-
     </>
   );
 };
