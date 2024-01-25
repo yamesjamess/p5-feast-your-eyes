cb4390d2bb3774bb1a5ae9ad0b4ce3094253af68
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-25 23:18:52 +0000

    feat: add rule modals for user to read the rules before creating an account

--- src/pages/auth/RegisterForm.js
+++ src/pages/auth/RegisterForm.js
@@ -13,6 +13,7 @@ import {
   Row,
   Container,
   Alert,
+  Modal,
 } from "react-bootstrap";
 
 import axios from "axios";
@@ -30,6 +31,11 @@ const RegisterForm = () => {
   const [errors, setErrors] = useState({});
 
   const history = useHistory();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const handleChange = (event) => {
     setRegisterData({
@@ -80,7 +86,6 @@ const RegisterForm = () => {
                 {message}
               </Alert>
             ))}
-
             <Form.Group controlId="password1">
               <Form.Label className="d-none">password</Form.Label>
               <Form.Control
@@ -97,7 +102,6 @@ const RegisterForm = () => {
                 {message}
               </Alert>
             ))}
-
             <Form.Group controlId="password2">
               <Form.Label className="d-none">confirm password</Form.Label>
               <Form.Control
@@ -115,6 +119,12 @@ const RegisterForm = () => {
               </Alert>
             ))}
 
+            <div className={styles.RulesContainer}>
+              click to read the&nbsp;
+              <span onClick={handleShow} className={styles.Rules}>
+                rules
+              </span>
+            </div>
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Dark}`}
               type="submit"
@@ -131,10 +141,109 @@ const RegisterForm = () => {
 
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
+            already have an account?{" "}
+            <span className={styles.UnderlineBold}>sign in</span>
           </Link>
         </Container>
       </Col>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className={`${styles.Header} ${styles.Dark}`}>
+            Feast Your Eyes Rules
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={appStyles.Content}>
+          <ol className={styles.OrderedList}>
+            <li>
+              Content Guidelines:
+              <ul className={styles.UnorderedList}>
+                <li>
+                  Users are allowed to upload and share images of food-related
+                  content only.
+                </li>
+                <li>
+                  Content that is not related to food, such as non-food items,
+                  promotional material, or unrelated images, is strictly
+                  prohibited.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Respectful Conduct:
+              <ul className={styles.UnorderedList}>
+                <li>
+                  Users are expected to engage in respectful and constructive
+                  conversations. Any form of harassment, hate speech, or
+                  inappropriate comments related to content or other users will
+                  not be tolerated.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Ownership and Attribution:
+              <ul className={styles.UnorderedList}>
+                <li>
+                  Users should only upload images that they own or have the
+                  right to share. Proper attribution should be given if the
+                  image belongs to someone else.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Community Standards:
+              <ul className={styles.UnorderedList}>
+                <li>
+                  Users are encouraged to contribute positively to the community
+                  by providing helpful comments, tips, and feedback on shared
+                  content.
+                </li>
+                <li>
+                  Any form of spamming, solicitation, or activities that disrupt
+                  the community's harmony are not allowed.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Moderation and Reporting:
+              <ul className={styles.UnorderedList}>
+                <li>
+                  The platform reserves the right to moderate content and may
+                  remove any content that violates the terms of usage.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Account Responsibility:
+              <ul className={styles.UnorderedList}>
+                <li>
+                  Users are responsible for maintaining the security of their
+                  accounts. Sharing login credentials or engaging in any
+                  activity that compromises the security of the platform is
+                  strictly prohibited.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Updates and Changes:
+              <ul className={styles.UnorderedList}>
+                <li>
+                  The terms of usage may be updated or modified to reflect
+                  changes in the platform's policies. Users will be notified of
+                  any significant changes.
+                </li>
+              </ul>
+            </li>
+          </ol>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            className={`${btnStyles.Button} ${btnStyles.Dark}`}
+            onClick={handleClose}
+          >
+            acknowledge
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Row>
   );
 };
