a1717d20093653e0aad9b763aff0e0e346d9b6a4
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-27 23:18:12 +0000

    chore: add comments to help explain code

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,9 +1,13 @@
 import { rest } from "msw";
 
+// Define the base URL for the API
 const baseURL = "https://fye-drf-api-6b84783d9a37.herokuapp.com/";
 
+// Array of request handlers for different API endpoints
 export const handlers = [
+  // Request handler for getting user information
   rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    // Return a mocked JSON response representing a user
     return res(
       ctx.json({
         pk: 2,
@@ -17,6 +21,8 @@ export const handlers = [
       })
     );
   }),
+  
+  // Request handler for simulating user logout
   rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
--- src/setupTests.js
+++ src/setupTests.js
@@ -6,8 +6,14 @@ import "@testing-library/jest-dom";
 import { setupServer } from "msw/node";
 import { handlers } from "./mocks/handlers";
 
+// Create a mock server using the setupServer function with the specified request handlers.
 const server = setupServer(...handlers);
 
+// Set up the mock server before running the tests.
 beforeAll(() => server.listen());
+
+// Reset the handlers between each test to ensure a clean state.
 afterEach(() => server.resetHandlers());
+
+// Close the server after all tests are completed.
 afterAll(() => server.close());
