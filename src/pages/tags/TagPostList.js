463f06e2858d5583a6d0370ac790e77efc699436
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-27 00:06:56 +0000

    feat: create TagPostList to display posts base on the tag that is being filtered

---
+++ src/pages/tags/TagPostList.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsListPage.module.css";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import Post from "../posts/Post";
+import Asset from "../../components/Asset";
+import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
+import TopRecommend from "../recommends/TopRecommend";
+import Tags from "./Tags";
+
+
+const TagPostList = ({message}) => {
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [posts, setPosts] = useState({ results: [] });
+  const { tag } = useParams();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/tag/${tag}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchData();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [tag, pathname]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles mobile />
+        <i className={`fa-solid fa-magnifying-glass ${styles.SearchIcon}`} />
+        
+        <TopRecommend message={message} />
+
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+        <Tags />
+      </Col>
+    </Row>
+  );
+};
+
+export default TagPostList;
